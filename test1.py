2a29ea799167f8b526e947313ac54d0c5e7ce6b4
AgneG25 <44949532+AgneG25@users.noreply.github.com>
2022-05-23 13:35:13 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,6 +7,7 @@ import subprocess
 subprocess.run('conda install -c conda-forge r-base', shell=True)
 
 !pip install rpy2
+RPY2_CFFI_MODE=ABI
 
 import subprocess
 subprocess.run('conda install -c conda-forge r-base', shell=True)
